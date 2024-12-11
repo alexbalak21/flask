e3ab3f3fc68039f45bf7bb23a8877b91cbbb41be
alexbalak21 <alex.balak@outlook.com>
2024-12-11 10:07:27 +0100

    new structure

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 from flask import Flask
 
 app = Flask(__name__)
-if __name__ == "__main__":
-    app.run(debug=True)
-    app.run(port=7000)
 
 @app.route("/")
 def hello_world():
-    
     return "<h1>Hello, World!</h1>"
+
